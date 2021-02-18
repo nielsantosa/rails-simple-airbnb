6d04ada380462bdd2255cb15f720862a7b792234
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-18 17:05:02 +0800

    Finish the index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Start seeding 🍑"
+
+puts "Cleaning up database 🧹"
+Flat.destroy_all
+
+5.times do
+  puts "Creating new flat 🏡"
+  Flat.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: (50..200).step(10).to_a.sample,
+    number_of_guests: (1..10).to_a.sample)
+end
+
+puts "Finish seeding 🍑"
